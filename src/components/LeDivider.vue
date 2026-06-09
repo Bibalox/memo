99a66e2d17a20d114acfa02a8e3b9e8ea07de597
JB Bal <8114915+Bibalox@users.noreply.github.com>
2026-06-09 18:26:53 +0200

    ✨ Create the divider

---
+++ src/components/LeDivider.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="le-divider">
+    <hr class="le-divider__stroke">
+  </div>
+</template>
+
+<style lang="scss">
+  .le-divider {
+    padding: var(--spacing-static-xs) var(--spacing-static-l);
+    width: 100%;
+
+    &__stroke {
+      color: var(--foreground-neutral-weaker);
+      height: var(--stroke-width);
+      width: 100%;
+    }
+  }
+</style>
